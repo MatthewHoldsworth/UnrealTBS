bcae96fc4ed39ca74a0fa46c0467296371b94370
MatthewHoldsworth <63724973+MatthewHoldsworth@users.noreply.github.com>
2024-08-20 15:00:08 +0100

    Healthbar added to party character, and have attached widget component to root

--- Source/TBS/Private/Character/TBSCharacter.cpp
+++ Source/TBS/Private/Character/TBSCharacter.cpp
@@ -13,6 +13,7 @@ ATBSCharacter::ATBSCharacter()
  	AttributeSet = CreateDefaultSubobject<UTBSAttributeSet>("AttributeSet");
 	WidgetComponent = CreateDefaultSubobject<UWidgetComponent>("WidgetComponent");
 	WidgetComponent->SetWidgetSpace(EWidgetSpace::Screen);
+	WidgetComponent->AttachToComponent(RootComponent, FAttachmentTransformRules::KeepRelativeTransform);
 }
 
 UAbilitySystemComponent* ATBSCharacter::GetAbilitySystemComponent() const
